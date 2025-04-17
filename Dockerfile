f0640764d41d5b1aa94fff68197a430b26945914
Swapnil <jadeswapnil2707@gmail.com>
2025-04-17 23:29:34 +0530

    update docker file for allure reports

--- Dockerfile
+++ Dockerfile
@@ -16,8 +16,14 @@ COPY . .
 # Install Java (if needed for your tests)
 RUN apt-get update && apt-get install -y openjdk-11-jre-headless
 
+# Install Allure
+RUN npm install -g allure-commandline --save-dev
+
+# Expose the port for Allure server
+EXPOSE 5000
+
 # Set JAVA_HOME
 ENV JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 
 # Default command to run tests
-CMD ["npm", "test"]
+CMD ["npm", "test", "run", "serve:report"]
---
+++ tests/swagLabs.spec.ts
@@ -0,0 +1,46 @@
+import {test, expect, Page} from "@playwright/test";
+import { PageObjectManager } from "../src/pages/PageObjectManager";
+
+let page: Page;
+let pom: PageObjectManager;
+
+test.beforeEach( async({browser}) =>{
+
+    page = await browser.newPage();
+    pom = new PageObjectManager(page);
+    await pom.getLoginPage().navigateToLoginPage();
+});
+
+test.afterEach( async () => {
+    
+    await pom.getHomePage().logOutApp();
+});
+
+test("Sauce Lab Appliaction End to End Flow @pom", async () => {
+
+    const loginpage = pom.getLoginPage();
+    const homepage = pom.getHomePage();
+    const checkoupage = pom.getCheckOutPage();
+
+    await test.step('Login to Sauce Labs Appliaction', async ()=> {
+
+        await loginpage.enterCredencials("standard_user", "secret_sauce");
+        await loginpage.clickLoginBtn();
+    });
+
+    await test.step('Add product to Cart', async ()=> {
+
+        await homepage.getTotalProductCount();
+        await homepage.getProductPrice('Sauce Labs Backpack');
+        await homepage.addproductToCart('Sauce Labs Backpack');
+    });
+
+
+    await test.step('Checkout Items', async () => {
+
+        await checkoupage.clickOnCheckOutBtn();
+        await checkoupage.validateErrorMsg();
+        await checkoupage.continueToCheckout();
+    });
+
+});
